2b54b390455158b17c8218c1ae2802ae5fdabf82
Taonce <Taonce@163.com>
2019-07-25 13:51:56 +0800

    暂时控制下启动页在锁屏状态下不跳转的问题

--- app/src/main/java/com/kuky/demo/wan/android/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/kuky/demo/wan/android/ui/splash/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.kuky.demo.wan.android.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import com.kuky.demo.wan.android.R
 import com.kuky.demo.wan.android.base.BaseFragment
@@ -11,18 +12,23 @@ import com.kuky.demo.wan.android.databinding.FragmentSplashBinding
  * @description 欢迎页
  */
 class SplashFragment : BaseFragment<FragmentSplashBinding>() {
+    companion object {
+        private val mHandler = Handler()
+    }
 
     override fun getLayoutId(): Int = R.layout.fragment_splash
 
-    override fun initFragment(view: View, savedInstanceState: Bundle?) {
+    override fun initFragment(view: View, savedInstanceState: Bundle?) {}
 
-        view.postDelayed({
+    override fun onResume() {
+        super.onResume()
+        // TODO 放在onResume中可以防止锁屏不跳转问题，发现更好的方案再替换
+        mHandler.postDelayed({
             mNavController.let {
                 val graph = it.navInflater.inflate(R.navigation.wan_navigation)
                     .apply { startDestination = R.id.mainFragment }
-
                 it.graph = graph
             }
-        }, 3000)
+        }, 3000L)
     }
 }
