0b19cc7c4fdb76e35404d7bb2e3de7b0bd4b277a
kuky <kukyxingshi@163.com>
2019-08-07 22:47:57 +0800

    splash 加入文字动画

--- app/src/main/java/com/kuky/demo/wan/android/ui/SplashActivity.kt
+++ app/src/main/java/com/kuky/demo/wan/android/ui/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.kuky.demo.wan.android.ui
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import com.kuky.demo.wan.android.R
 import com.kuky.demo.wan.android.base.BaseActivity
 import com.kuky.demo.wan.android.databinding.ActivitySplashBinding
+import kotlinx.android.synthetic.main.activity_splash.*
+import yanzhikai.textpath.PathAnimatorListener
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
 
@@ -16,12 +19,24 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>() {
     override fun getLayoutId(): Int = R.layout.activity_splash
 
     override fun initActivity(savedInstanceState: Bundle?) {
-        mHandler.postDelayed({
-            startActivity(
-                Intent(this@SplashActivity, MainActivity::class.java)
-            )
-            finish()
-        }, 3000)
+        enjoy.let {
+            it.startAnimation(0f, 1f)
+            it.setDuration(2000)
+            it.setAnimatorListener(object : PathAnimatorListener() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+
+                    mHandler.postDelayed({
+                        startActivity(
+                            Intent(this@SplashActivity, MainActivity::class.java)
+                        )
+                        finish()
+                    }, 500)
+                }
+            })
+        }
+
+
     }
 
     override fun onDestroy() {
