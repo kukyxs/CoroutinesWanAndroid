4de69d1bf1bdc18df0bd3e6ae09cb4eaecdfc07f
kuky <kukyxingshi@163.com>
2019-11-11 14:33:42 +0800

    增加网络不可用通知用户网络不可用

--- app/src/main/java/com/kuky/demo/wan/android/ui/MainActivity.kt
+++ app/src/main/java/com/kuky/demo/wan/android/ui/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.kuky.demo.wan.android.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
 import android.os.Bundle
 import com.kuky.demo.wan.android.R
 import com.kuky.demo.wan.android.base.BaseActivity
@@ -8,11 +12,45 @@ import com.kuky.demo.wan.android.databinding.ActivityMainBinding
 import com.kuky.demo.wan.android.ui.main.MainFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+    private var availableCount = 0
+
+    private val manager: ConnectivityManager by lazy {
+        getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    private val request: NetworkRequest by lazy {
+        NetworkRequest.Builder().build()
+    }
+
+    private val netStateCallback: ConnectivityManager.NetworkCallback by lazy {
+        object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                availableCount++
+                checkState()
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                availableCount--
+                checkState()
+            }
+        }
+    }
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
     override fun initActivity(savedInstanceState: Bundle?) {
+        manager.registerNetworkCallback(request, netStateCallback)
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        manager.unregisterNetworkCallback(netStateCallback)
+    }
+
+    private fun checkState() {
+        mBinding.netAvailable = availableCount > 0
     }
 
     override fun needTransparentStatus(): Boolean = true
